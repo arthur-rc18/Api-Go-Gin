cec2107ba134a7fb35f4da94a6e914f5a4635e18
arthur-rc18 <arthurrodri015@gmail.com>
2022-04-25 09:32:08 -0300

    Making the connection to the database

---
+++ database/db.go
@@ -0,0 +1,25 @@
+package database
+
+import (
+	"log"
+
+	"github.com/arthur-rc18/Api-Go-Gin/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	DB  *gorm.DB
+	err error
+)
+
+func DbConnection() {
+
+	connectionString := "host=localhost user=root password=root dbname=root port=54321 sslmode=disable"
+	DB, err = gorm.Open(postgres.Open(connectionString))
+
+	if err != nil {
+		log.Panic("Connection to database failed")
+	}
+	DB.AutoMigrate(&models.Knight{})
+}
