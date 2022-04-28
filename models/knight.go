c1fe7cc0c94fc38ac310f7ca15d1d3a4dfd68ebf
arthur-rc18 <arthurrodri015@gmail.com>
2022-04-28 08:48:06 -0300

    Adding swaggertype in the struct

--- models/knight.go
+++ models/knight.go
@@ -1,10 +1,22 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gopkg.in/validator.v2"
+	"gorm.io/gorm"
+)
 
 type Knight struct {
 	gorm.Model
-	Name            string `json:"name"`
-	Weapon          string `json:"weapon"`
-	Characteristics string `json:"caracteristic"`
+	// Specifying the field's characteristics using validate
+	Name            string `json:"name" validate:"nonzero"`
+	Weapon          string `json:"weapon" validate:"len=20"`
+	Characteristics string `json:"characteristic" validate:"len=1000"`
+	Age             string `json:"age" validate:"len=4, regexp=^[0-9]*$" swaggertype:"string"`
+}
+
+func ValidateKnight(knight *Knight) error {
+	if err := validator.Validate(knight); err != nil {
+		return err
+	}
+	return nil
 }
